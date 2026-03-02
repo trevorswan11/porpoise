11eee53782e7b0b382591c8c0e1d9755b9cd495b
trevorswan11 <tcs94@case.edu>
2026-03-02 13:31:50 -0500

    Provides a quick hacky fix for #26 while not interfering with any other platform.
    Unfortunate that this is needed in the first place.

--- flake.nix
+++ flake.nix
@@ -13,15 +13,31 @@
       let
         pkgs = import nixpkgs { inherit system; };
       in
+      with pkgs;
       {
-        devShells.default = pkgs.mkShell {
+        devShells.default = mkShell {
           hardeningDisable = [ "all" ];
-          buildInputs = with pkgs; [
+          buildInputs = [
             zig_0_15
             zls_0_15
             llvmPackages_21.clang-tools
             llvmPackages_21.lldb
           ];
+
+          # Required for LLDB on macOS (stinky)
+          shellHook = lib.optionalString stdenv.isDarwin ''
+            # Tested both paths on my machine and they both work (adds some flexibility)
+            if [[ -z "$LLDB_DEBUGSERVER_PATH" ]]; then
+              XCODE_PATH="/Applications/Xcode.app/Contents/SharedFrameworks/LLDB.framework/Versions/A/Resources/debugserver"
+              CLT_PATH="/Library/Developer/CommandLineTools/Library/PrivateFrameworks/LLDB.framework/Versions/A/Resources/debugserver"
+
+              if [[ -f "$XCODE_PATH" ]]; then
+                export LLDB_DEBUGSERVER_PATH="$XCODE_PATH"
+              elif [[ -f "$CLT_PATH" ]]; then
+                export LLDB_DEBUGSERVER_PATH="$CLT_PATH"
+              fi
+            fi
+          '';
         };
       }
     );
