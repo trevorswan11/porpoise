3fec94a0caf1f728f695ac86288da0ca93a3e5a6
nobody_on_earth <reddeadarthurfan1889@gmail.com>
2026-03-11 16:41:22 -0500

    Update main.cpp

--- apps/cli/src/main.cpp
+++ apps/cli/src/main.cpp
@@ -1,3 +1,9 @@
 #include "program.hpp"
 
-auto main() -> int { conch::cli::Program::interactive(); }
+/* Previous line was: auto main() -> int { conch::cli::Program::interactive(); } 
+
+However, the "-> int" part on an auto function was verbose. Also, I spread out the function for better readability.
+*/
+int main() {
+  conch::cli::Program::interactive();
+}
